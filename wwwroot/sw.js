78bb94e0cd016cfdb6b8c806db36e666151845c4
waybill2026-jpg <waybill2026@gmail.com>
2026-04-18 17:21:08 +0500

    Delete wwwroot directory

--- wwwroot/sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('ardmus-v1').then((cache) => cache.addAll(['index.html'])));
-});
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
-});
