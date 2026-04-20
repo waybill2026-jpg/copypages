050024c0a3954b63e2e15f5ac46244bdebafa0a4
waybill2026-jpg <waybill2026@gmail.com>
2026-04-20 16:10:55 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,18 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('ardmus-v1').then((cache) => cache.addAll(['index.html'])));
+const cacheName = 'ardmus-v1';
+const assets = ['./', './index.html', './menu.html', './logo.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(assets);
+    })
+  );
 });
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
